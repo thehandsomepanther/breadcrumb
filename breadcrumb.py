21f0b2c24668fd5a63ca6864413ff8f278906254
Joshua Shi <thehandsomepanther@gmail.com>
2015-12-27 23:01:20 -0500

    restructured enrollment counts

--- breadcrumb.py
+++ breadcrumb.py
@@ -4,36 +4,34 @@ import csv
 datafile = 'crouton.csv'
 enrollmentsfile = 'enrollments.txt'
 
-def calc_average_enrollment(current_enrollment, average_enrollment, num_terms):
-	return (float(current_enrollment) + float(average_enrollment) * (num_terms - 1)) / num_terms
-
-def calc_growth(current_enrollment, previous_enrollment):
-	return (float(current_enrollment) - float(previous_enrollment)) / float(previous_enrollment)
+def calc_average_enrollment(enrollment_list):
+	total = 0
+	for key in enrollment_list:
+		total += float(enrollment_list[key])
+	return total / len(enrollment_list.keys())
 
 with open(datafile, 'rb') as csvfile:
 	reader = csv.DictReader(csvfile)
 	course_dict = {}
 	for row in reader:
 		course_key = "{} {}-{} {}".format(row['academic_subject_code'], row['course_number'], row['course_subnum'], row['course_name'])
-		print course_key
 		course_term = "{} {}".format(row['academic_quarter'], row['academic_year'])
 
 		if course_key in course_dict.keys():
 			this_course = course_dict[course_key]
 
-			this_course['growth'].append(calc_growth(row['enrollment_count'], this_course['enrollment_counts'][-1]))
-			this_course['total_growth'] += calc_growth(row['enrollment_count'], this_course['enrollment_counts'][-1])
+			if course_term in this_course['enrollment_list'].keys():
+				this_course['enrollment_list'][course_term] += int(row['enrollment_count'])
+			else:
+				this_course['enrollment_list'][course_term] = int(row['enrollment_count'])
 
-			this_course['enrollment_counts'].append(int(row['enrollment_count']))
-			this_course['enrollment_terms'].append(course_term)
-			this_course['average_enrollment'] = calc_average_enrollment(row['enrollment_count'], this_course['average_enrollment'], len(this_course['enrollment_counts']))
+			this_course['average_enrollment'] = calc_average_enrollment(this_course['enrollment_list'])
 		else:
 			course_dict[course_key] = {
 				'average_enrollment' : int(row['enrollment_count']),
-				'enrollment_counts' : [int(row['enrollment_count'])],
-				'enrollment_terms' : [course_term],
-				'growth' : [],
-				'total_growth': 0
+				'enrollment_list' : {
+					course_term : int(row['enrollment_count'])
+				}
 			}
 
 	with open(enrollmentsfile, 'w') as outfile:
